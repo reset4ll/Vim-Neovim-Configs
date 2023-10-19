0a4b63523a8b5b65136c50c06f64a0d0b762816c
reset4ll <reset4ll@users.noreply.github.com>
2023-10-19 20:10:22 +0200

    Update: enable orgmode

--- init.lua
+++ init.lua
@@ -172,6 +172,7 @@ require('lazy').setup({
   require 'custom.plugins.autopairs',
   require 'custom.plugins.devicons',
   require 'custom.plugins.timeout',
+  require 'custom.plugins.orgmode',
   
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
